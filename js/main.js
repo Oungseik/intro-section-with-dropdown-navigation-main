38a96e9e4c1340dbaa9b1c9348fa73f2aaa3dea4
Oungseik <mhemaungthuwin@gmail.com>
2022-05-15 16:53:36 +0630

    finish features toggle

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 const feature = $(".feature");
 feature.on("click", () => {
     feature.toggleClass("active");
+    feature.hasClass("active")
+        ? feature.children().removeClass("down").addClass("up")
+        : feature.children().removeClass("up").addClass("down");
+    $(".feature-items").slideToggle(200);
 });
 
 $(".menuBtn").on("click", function () {
