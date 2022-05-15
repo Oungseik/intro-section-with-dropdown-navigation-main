acc7af1300e6bf435c57b1a2d929ad2f2afd5d0b
Oungseik <mhemaungthuwin@gmail.com>
2022-05-15 10:36:07 +0630

    finish menu btn

--- js/main.js
+++ js/main.js
@@ -2,3 +2,7 @@ const feature = $(".feature");
 feature.on("click", () => {
     feature.toggleClass("active");
 });
+
+$(".menuBtn").on("click", function () {
+    $(this).toggleClass("active");
+});
